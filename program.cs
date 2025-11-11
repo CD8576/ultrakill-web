e0e54a5524bace33acb21cf5bb32fb2c5b84de40
CD8576 <148923376+CD8576@users.noreply.github.com>
2025-11-11 15:18:38 -0600

    Update program.cs

--- program.cs
+++ program.cs
@@ -5,6 +5,10 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+// Serve static files from wwwroot
+app.UseDefaultFiles(); // looks for index.html by default
+app.UseStaticFiles();
+
 // Default route at /
 app.MapGet("/", () => "Hello! The API is running.");
 
