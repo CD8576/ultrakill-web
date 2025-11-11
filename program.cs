6cdf0db5eff0725fcc0d3f0dac2e9bd9aa17b301
CD8576 <148923376+CD8576@users.noreply.github.com>
2025-11-11 14:15:38 -0600

    Initialize ASP.NET Core application in program.cs

---
+++ program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add controllers
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Map controllers
+app.MapControllers();
+
+app.Run();
